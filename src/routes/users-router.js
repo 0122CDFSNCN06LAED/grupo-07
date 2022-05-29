c435a2bc1a86900aa1d7a5cdaaed4859f7b9228b
Carlos Gordillo Olivera <gordillocarlosn@gmail.com>
2022-05-28 23:54:48 -0300

    Se agrega multer para cargar imagen de avatar

--- src/routes/users-router.js
+++ src/routes/users-router.js
@@ -1,9 +1,27 @@
 const { Router } = require("express");
 const router = Router();
+const path = require('path')
+const multer = require('multer')
 const userController = require("../controllers/users-controller");
+const regLogMiddelware = require('../middlewares/regLogValidations')
+
+// Configuración de multer
+const storage = multer.diskStorage({
+    destination: (req,file,cb) =>{
+        cb(null, path.join(__dirname,'../public/img/user-img'))
+    },
+    filename: (req,file,cb) => {
+        const newFileName = 'user-' + Date.now() + path.extname(file.originalname)
+        cb(null, newFileName)
+    }
+})
+const upload = multer({storage: storage})
+
+// Loguear un usuario
+router.get("/login", userController.login);
 
 /*** GET AN USER ***/
-router.get("/user-profile", userController.userProfile);
+router.get("/user-profile" ,userController.userProfile);
 
 /*** EDIT AN USER ***/
 router.put("/:id", userController.update);
@@ -12,6 +30,9 @@ router.put("/:id", userController.update);
 router.delete('/:id', userController.destroy);
 
 /*** CREATE AN USER ***/
-router.post('/',userController.store);
+router.get("/register", userController.register);
+router.post('/',upload.single('profile_pic'),regLogMiddelware('register'),userController.store);
+
+
 
 module.exports = router;
