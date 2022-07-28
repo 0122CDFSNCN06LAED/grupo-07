8f7639c7e600cbf16671fc4a6d272b2992e9e9e9
carlosgordillo-tc <104100941+carlosgordillo-tc@users.noreply.github.com>
2022-07-27 21:20:00 -0300

    Se modifica script base de datos

--- tables_creation.sql
+++ tables_creation.sql
@@ -27,6 +27,18 @@ INSERT INTO users VALUES
 (9, 'Julian','Gates','julig','juligates@gmail.com','12345275256','2004-06-28',NULL),
 (10, 'Francisco','Franco','franfran','franfran@gmail.com','12368638634793456','1983-04-30',NULL);
 
+DROP TABLE IF EXISTS categories;
+CREATE TABLE categories(
+    id int unsigned NOT NULL AUTO_INCREMENT,
+    cat varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+    primary key (id)
+);
+
+INSERT INTO categories VALUES
+(1,'SUV'),
+(2,'Sedan'),
+(3, 'Hatchback'),
+(4,'Pickup');
 
 DROP TABLE IF EXISTS products;
 CREATE TABLE products (
@@ -36,23 +48,25 @@ model varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 manufacture_year int NOT NULL,
 color varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 door_number TINYINT NOT NULL,
+category TINYINT NOT NULL,
 transmission varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 motor_type varchar(50) COLLATE utf8_unicode_ci NOT NULL,
 description varchar(200) COLLATE utf8_unicode_ci,
 price int NOT NULL,
 discount int,
 image varchar(200),
-primary key (id)
+primary key (id),
+key products_category_foreign (category)
 );
 
 INSERT INTO products VALUES
-(1, 'Ford', 'K', 2004, 'Rojo', 3, 'Manual', 'GNC', 'Lindo auto', 23000, 10, 'auto.jpg'),
-(2, 'Chevrolet', 'Onix', 2005, 'Azul', 5, 'Manual', 'Nafta', NULL, 17000,2, 'auto2.jpg'),
-(3, 'Ford', 'Ranger', 2006, 'Negro', 5, 'Manual', 'Nafta', 'Comfortable', 20000,15, 'auto3.jpg'),
-(4, 'Audi', 'Q3', 2002, 'Negro', 3, 'Manual', 'Diesel', 'Precioso', 53000,23, 'auto4.jpg'),
-(5, 'BWM', 'X5', 2000, 'Blanco', 3, 'Manual', 'GNC', 'Atractivo', 30000,12,'auto5.jpg' ),
-(6, 'Peugeot', '2012', 2004, 'Azul', 5, 'Automatico', 'Diesel', NULL, 16000,14, 'auto3.jpg'),
-(7, 'Audi', 'A5', 2021, 'Gris', 3, 'Automatico', 'Nafta', NULL, 80000,15, 'auto.jpg');
+(1, 'Ford', 'K', 2004, 'Rojo', 3,1, 'Manual', 'GNC', 'Lindo auto', 23000, 10, 'auto.jpg'),
+(2, 'Chevrolet', 'Onix', 2005, 'Azul', 5,2, 'Manual', 'Nafta', NULL, 17000,2, 'auto2.jpg'),
+(3, 'Ford', 'Ranger', 2006, 'Negro', 5,3 ,'Manual', 'Nafta', 'Comfortable', 20000,15, 'auto3.jpg'),
+(4, 'Audi', 'Q3', 2002, 'Negro', 3,4, 'Manual', 'Diesel', 'Precioso', 53000,23, 'auto4.jpg'),
+(5, 'BWM', 'X5', 2000, 'Blanco', 3,4, 'Manual', 'GNC', 'Atractivo', 30000,12,'auto5.jpg' ),
+(6, 'Peugeot', '2012', 2004, 'Azul', 5, 2 ,'Automatico', 'Diesel', NULL, 16000,14, 'auto3.jpg'),
+(7, 'Audi', 'A5', 2021, 'Gris', 3, 1,'Automatico', 'Nafta', NULL, 80000,15, 'auto.jpg');
 
 DROP TABLE IF EXISTS users_products;
 CREATE TABLE users_products(
@@ -62,8 +76,8 @@ product_id int unsigned NOT NULL,
 primary key (id),
 key users_products_user_id_foreign (user_id),
 key users_products_product_id_foreign (product_id),
-constraint users_products_user_id_foreign foreign key (user_id) references users (id) ON UPDATE CASCADE,
-constraint users_products_product_id_foreign foreign key (product_id) references products (id) ON UPDATE CASCADE
+constraint users_products_user_id_foreign foreign key (user_id) references users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+constraint users_products_product_id_foreign foreign key (product_id) references products (id) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 INSERT INTO users_products VALUES
